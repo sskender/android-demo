f7a5044b7f7b4e9dbe22fc78e8f48c56bdabd9a5
sskender <sskender@users.noreply.github.com>
2019-03-20 18:00:13 +0100

    edit_user_info design

---
+++ app/src/main/java/com/example/sskender/demoapp/EditUserInfo.kt
@@ -0,0 +1,13 @@
+package com.example.sskender.demoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditUserInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edituserinfo)
+    }
+
+}
