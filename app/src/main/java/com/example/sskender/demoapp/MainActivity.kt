b7277880e853d1a4b5da864eb77e94ed35708778
sskender <sskender@users.noreply.github.com>
2019-03-20 17:35:57 +0100

    activity_main design update

--- app/src/main/java/com/example/sskender/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/sskender/demoapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.sskender.demoapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sskender.demoapp.model.UserInfo.firstName
+import com.example.sskender.demoapp.model.UserInfo.lastName
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstNameTextView.text = firstName
+        lastNameTextView.text = lastName
+
     }
+
 }
---
+++ app/src/main/java/com/example/sskender/demoapp/model/UserInfo.kt
@@ -0,0 +1,8 @@
+package com.example.sskender.demoapp.model
+
+object UserInfo {
+
+    var firstName: String? = "John"
+    var lastName: String? = "Doe"
+
+}
